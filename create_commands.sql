2bc6fd3151dbadbdf992c6869f98e32815c0cf0a
matteogiorla <matteo.giorla@me.com>
2017-04-18 14:40:03 +0200

    Update of the create commands with correct VARCHAR/CHAR/TEXT

--- create_commands.sql
+++ create_commands.sql
@@ -2,14 +2,14 @@
 
 CREATE TABLE Story_Type(
 	id INTEGER NOT NULL,
-	name CHAR(40) NOT NULL, -- constraint assumption
+	name VARCHAR NOT NULL, -- constraint assumption
 	PRIMARY KEY(id)
 );
 
 CREATE TABLE Language(
 	id INTEGER NOT NULL,
-	code CHAR(3) NOT NULL, -- constraint assumption
-	name CHAR(27) NOT NULL, -- constraint assumption
+	code CHAR(4) NOT NULL, -- constraint assumption
+	name VARCHAR NOT NULL, -- constraint assumption
 	PRIMARY KEY(id)
 );
 
@@ -22,12 +22,12 @@ CREATE TABLE Series_Publication_Type(
 -- contains relation publ_country
 CREATE TABLE Publisher(
 	id INTEGER NOT NULL,
-	name CHAR(124) NOT NULL, -- constraint assumption
+	name VARCHAR NOT NULL, -- constraint assumption
 	country_id INTEGER NOT NULL, -- relation constraint (total participation)
 	year_began INTEGER,
 	year_ended INTEGER,
-	notes CHAR(3201),
-	url CHAR(130),
+	notes TEXT,
+	url VARCHAR,
 	PRIMARY KEY(id),
 	FOREIGN KEY(country_id) REFERENCES Country(id)
 );
@@ -35,11 +35,11 @@ CREATE TABLE Publisher(
 -- contains relation bg_publisher
 CREATE TABLE Brand_Group(
 	id INTEGER NOT NULL,
-	name CHAR(94) NOT NULL, -- constraint assumption
+	name VARCHAR NOT NULL, -- constraint assumption
 	year_began INTEGER,
 	year_ended INTEGER,
-	notes CHAR(729),
-	url CHAR(152),
+	notes TEXT,
+	url VARCHAR,
 	publisher_id INTEGER NOT NULL, -- relation constraint (total participation)
 	PRIMARY KEY(id),
 	FOREIGN KEY(publisher_id) REFERENCES Publisher(id)
@@ -48,14 +48,14 @@ CREATE TABLE Brand_Group(
 -- contains relations ind_country and ip_publisher
 CREATE TABLE Indicia_Publisher(
 	id INTEGER NOT NULL,
-	name CHAR(124) NOT NULL, -- constraint assumption
+	name VARCHAR NOT NULL, -- constraint assumption
 	publisher_id INTEGER NOT NULL, -- relation constraint (total participation)
 	country_id INTEGER NOT NULL, -- relation constraint (total participation)
 	year_began INTEGER,
 	year_ended INTEGER,
 	is_surrogate BIT NOT NULL, -- constraint assumption -- 0 or 1 value
-	notes CHAR(2593),
-	url CHAR(115),
+	notes TEXT,
+	url VARCHAR,
 	PRIMARY KEY(id),
 	FOREIGN KEY(country_id) REFERENCES Country(id),
 	FOREIGN KEY(publisher_id) REFERENCES Publisher(id)
@@ -64,49 +64,29 @@ CREATE TABLE Indicia_Publisher(
 CREATE TABLE Country(
 	id INTEGER NOT NULL,
 	code CHAR(4) NOT NULL, -- constraint assumption
-	name CHAR(36) NOT NULL, -- constraint assumption
+	name VARCHAR NOT NULL, -- constraint assumption
 	PRIMARY KEY(id)
-);
-
--- contains relations story_target and story_origin
-CREATE TABLE Story_Reprint(
-	id INTEGER NOT NULL,
-	origin_id INTEGER NOT NULL, -- relation constraint (total participation)
-	target_id INTEGER NOT NULL, -- relation constraint (total participation)
-	PRIMARY KEY(id),
-	FOREIGN KEY(origin_id) REFERENCES Story(id),
-	FOREIGN KEY (target_id) REFERENCES Story(id)
-);
-
--- contains relations issue_target and issue_origin
-CREATE TABLE Issue_Reprint(
-	id INTEGER NOT NULL,
-	origin_issue_id INTEGER NOT NULL, -- relation constraint (total participation)
-	target_issue_id INTEGER NOT NULL, -- relation constraint (total participation)
-	PRIMARY KEY(id),
-	FOREIGN KEY(origin_issue_id) REFERENCES Issue(id),
-	FOREIGN KEY(target_issue_id) REFERENCES Issue(id)
 );
 
 -- contains relations written_in, first_issue, last_issue, publ_type, country and published_by
 CREATE TABLE Series(
 	id INTEGER NOT NULL,
-	name CHAR(239) NOT NULL, -- constraint assumption
-	format CHAR(200),
-	year_began INTEGER NOT NULL, -- constraint assumption
-	year_ended INTEGER,
-	publication_dates DATE,
+	name VARCHAR NOT NULL, -- constraint assumption
+	format VARCHAR,
+	year_began DATE NOT NULL, -- constraint assumption
+	year_ended DATE,
+	publication_dates VARCHAR,
 	first_issue_id INTEGER NOT NULL, -- relation constraint (total participation)
 	last_issue_id INTEGER NOT NULL, -- relation constraint (total participation)
 	publisher_id INTEGER NOT NULL, -- relation constraint (total participation)
 	country_id INTEGER NOT NULL, -- relation constraint (total participation)
 	language_id INTEGER NOT NULL, -- relation constraint (total participation)
-	notes CHAR(3868),
-	color CHAR(200),
-	dimensions CHAR(709),
-	paper_stock CHAR(140),
-	binding CHAR(90),
-	publishing_format CHAR(93),
+	notes TEXT,
+	color VARCHAR,
+	dimensions VARCHAR,
+	paper_stock VARCHAR,
+	binding VARCHAR,
+	publishing_format VARCHAR,
 	publication_type_id INTEGER, -- relation constraint (partial participation)
 	PRIMARY KEY(id),
 	FOREIGN KEY(first_issue_id) REFERENCES Issue(id),
@@ -120,21 +100,21 @@ CREATE TABLE Series(
 -- contains relations series, indicia_publ and issue_reprint
 CREATE TABLE Issue(
 	id INTEGER NOT NULL,
-	number INTEGER NOT NULL, -- constraint assumption
+	number VARCHAR,
 	series_id INTEGER NOT NULL, -- relation constraint (total participation)
 	indicia_publisher_id INTEGER, -- relation constraint (partial participation)
 	publication_date DATE,
-	price CHAR(220),
+	price VARCHAR,
 	page_count INTEGER,
-	indicia_frequency CHAR(134),
-	editing CHAR(1523),
-	notes CHAR(2897),
-	isbn CHAR(32),
+	indicia_frequency VARCHAR,
+	editing TEXT,
+	notes TEXT,
+	isbn VARCHAR,
 	valid_isbn CHAR(13),
-	barcode CHAR(38),
-	title CHAR(111),
+	barcode VARCHAR,
+	title VARCHAR,
 	on_sale_date DATE,
-	rating CHAR(118),
+	rating VARCHAR,
 	reprint_origin_id INTEGER,
 	PRIMARY KEY(id),
 	FOREIGN KEY(series_id) REFERENCES Series(id),
@@ -145,15 +125,15 @@ CREATE TABLE Issue(
 -- contains relations issue, story_type and story_reprint
 CREATE TABLE Story(
 	id INTEGER NOT NULL,
-	title CHAR(19727),
-	feature CHAR(423),
+	title VARCHAR,
+	feature VARCHAR,
 	issue_id INTEGER NOT NULL, -- relation constraint (total participation)
-	letters CHAR(776),
-	editing CHAR(347),
-	genre CHAR(119),
-	synopsis CHAR(17655),
-	reprint_notes CHAR(3562),
-	notes CHAR(11486),
+	letters VARCHAR,
+	editing VARCHAR,
+	genre VARCHAR,
+	synopsis TEXT,
+	reprint_notes TEXT,
+	notes TEXT,
 	type_id INTEGER NOT NULL, -- relation constraint (total participation)
 	reprint_origin_id INTEGER,
 	PRIMARY KEY(id),
