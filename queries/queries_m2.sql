5ba6f69cae5f3affa55b12b9b5a949114126cf7d
matteogiorla <matteo.giorla@me.com>
2017-04-19 12:57:44 +0200

    Reorganized data

---
+++ queries/queries_m2.sql
@@ -0,0 +1,36 @@
+-- a)  Print the brand group names with the highest number of Belgian indicia publishers.
+
+
+
+
+-- b)  Print the ids and names of publishers of Danish book series.
+
+
+
+
+-- c)  Print the names of all Swiss series that have been published in magazines.
+
+
+
+
+-- d)  Starting from 1990, print the number of issues published each year.
+
+
+
+
+-- e)  Print the number of series for each indicia publisher whose name resembles ‘DC comics’.
+
+
+
+
+-- f)  Print the titles of the 10 most reprinted stories
+
+
+
+
+-- g)  Print the artists that have scripted, drawn, and colored at least one of the stories they were involved in.
+
+
+
+
+-- h)  Print all non-reprinted stories involving Batman as a non-featured character.
