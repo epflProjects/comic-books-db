7bd0f6df04b32c4d5a242fd21b6c51e6c4496178
matteogiorla <matteo.giorla@me.com>
2017-05-04 11:19:32 +0200

    Queries list for final deliverable

---
+++ queries/queries_m3.sql
@@ -0,0 +1,57 @@
+-- a)  Print the series names that have the highest number of issues which contain a story whose type (e.g.,cartoon) is not the one occurring most frequently in the database (e.g, illustration). 
+
+
+
+-- b)  Print the names of publishers who have series with all series types. 
+
+
+
+-- c)  Print the 10 most-reprinted characters from Alan Moore's stories. 
+
+
+
+-- d)  Print the writers (scripts) of nature-related stories that have also done the pencilwork in all their nature-related stories. 
+
+
+
+-- e)  For each of the top-10 publishers in terms of published series, print the 3 most popular languages of their series. 
+
+
+
+-- f)  Print the languages that have more than 10000 original stories published in magazines, along with the number of those stories. 
+
+
+
+-- g)  Print all story types that have not been published as a part of Italian magazine series. 
+
+
+
+-- h)  Print the writers (scripts) of cartoon stories who have worked as writers for more than one indicia publisher. 
+
+
+
+-- i)  Print the 10 brand groups with the highest number of indicia publishers. 
+
+
+
+-- j)  Print the average series length (in terms of years) per indicia publisher. 
+
+
+
+-- k)  Print the top 10 indicia publishers that have published the most single-issue series. 
+
+
+
+-- l)  Print the 10 indicia publishers with the highest number of script writers in a single story. 
+
+
+
+-- m)  Print all Marvel heroes that appear in Marvel-DC story crossovers. 
+
+
+
+-- n)  Print the top 5 series with most issues 
+
+
+
+-- o)  Given an issue, print its most reprinted story.
