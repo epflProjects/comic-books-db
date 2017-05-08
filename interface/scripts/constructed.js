496408971393390321b28be0397500473716660f
bnanchen <nanchenbastian@gmail.com>
2017-05-08 15:39:42 +0200

    Creation of 'constructed.js' file and the client handles correctly the result of the constructed queries

---
+++ interface/scripts/constructed.js
@@ -0,0 +1,56 @@
+/**
+ * Constructed Query
+ */
+
+function constructedQuery() {
+    if ($(event.target).is("#given_search_brand_group")) {
+        queryResult('belgian');
+    } else if ($(event.target).is("#given_search_danish_publishers")) {
+        queryResult('danish');
+    } else if ($(event.target).is("#given_search_swiss_series")) {
+        queryResult('swiss');
+    } else if ($(event.target).is("#given_search_since_1990")) {
+        queryResult('1990');
+    } else if ($(event.target).is("#given_search_DC_comics")) {
+        queryResult('dcComics');
+    } else if ($(event.target).is("#given_search_10_most_reprinted")) {
+        queryResult('reprinted');
+    } else if ($(event.target).is("#given_search_artists")) {
+        queryResult('artist');
+    } else if ($(event.target).is("#given_search_Batman_non_reprinted")) {
+        queryResult('batman');
+    }
+}
+
+function queryResult(httpQuery) {
+    // "clean" the table
+    $("#output_table tr").each(function() {
+        this.remove();
+    });
+    $("#output_table td").each(function() {
+        this.remove();
+    });
+
+    $.get('constructed?q='+httpQuery, function(data) {
+        var table = $("#output_table");
+        var stringToAppend;
+
+        table.append("<thead>");
+        stringToAppend = "<tr>";
+        for (var i in data.attributes_name) {
+            stringToAppend += "<th>"+data.attributes_name[i]+"</th>";
+        }
+        stringToAppend += "</tr>";
+        table.append(stringToAppend);
+        table.append("</thead>");
+
+        for (var i in data.rows) {
+            stringToAppend = "<tr>";
+            for (var j in data.attributes_name) {
+                stringToAppend += "<td>"+data.rows[i][data.attributes_name[j]]+"</td>";
+            }
+            stringToAppend += "</tr>";
+            table.append(stringToAppend);
+        }
+    });
+}
--- interface/scripts/main.js
+++ interface/scripts/main.js
@@ -3,18 +3,7 @@
  */
 
 $(document).ready(function () {
-    $("#given_search_since_1990").click(function() {
-        // "clean" the table
-        $("#output_table td").each(function() {
-            this.remove();
-        });
-
-        $.get('constructed?q=1990', function(data) {
-            console.log(data);
-            var table = $("#output_table");
-            $.each(data, function(i, row) {
-                table.append("<tr><td>"+ row.publication_date +"</td></tr>")
-            })
-        });
+    $("#constructed_query_table").click(function() {
+        constructedQuery();
     });
 });
