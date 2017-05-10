5560a383cfe47133fa8f496ff5c5e84715a4d0da
bnanchen <nanchenbastian@gmail.com>
2017-05-10 10:26:07 +0200

    Creation of search.js file

---
+++ interface/scripts/search.js
@@ -0,0 +1,8 @@
+function postSearch(event) {
+    var target = $(event.currentTarget);
+    var val = target.attr('data-value');
+    var input = target.find('input');
+    var index;
+
+    // TODO https://codepen.io/bseth99/pen/fboKH?editors=0010#0
+}
